765c74b5409d80bc1ae0f4418fef5eb0bbcad9e4
Risang Baskoro <risangbaskoro@gmail.com>
2023-02-27 10:56:25 +0700

    Praktikum 1

---
+++ Praktikum 1/Titik.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2023.
+ * Risang Baskoro
+ */
+
+public class Titik {
+
+    private double absis;
+    private double ordinat;
+
+    public static int counterTitik = 0;
+
+    public Titik(double absis, double ordinat) {
+        this.absis = absis;
+        this.ordinat = ordinat;
+        counterTitik++;
+    }
+
+    public Titik() {
+        this.absis = 0;
+        this.ordinat = 0;
+        counterTitik++;
+    }
+
+    public double getAbsis() {
+        return absis;
+    }
+
+    public double getOrdinat() {
+        return ordinat;
+    }
+
+    public void setAbsis(double absis) {
+        this.absis = absis;
+    }
+
+    public void setOrdinat(double ordinat) {
+        this.ordinat = ordinat;
+    }
+
+    public int getCounterTitik() {
+        return counterTitik;
+    }
+}
